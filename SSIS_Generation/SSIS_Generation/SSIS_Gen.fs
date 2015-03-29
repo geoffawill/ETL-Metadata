de4041447e95fdc36b41b4ba3a486a46520936d2
geoffawill <geoff.williams@gmail.com>
2015-03-29 10:36:21 -0400

    F# SSIS Generator
    First pass at using F# to generate SSIS Packages

---
+++ SSIS_Generation/SSIS_Generation/SSIS_Gen.fs
@@ -0,0 +1,15 @@
+﻿// SSIS_Gen.fs
+// Auto Generation of SSIS Package
+
+open System.IO
+open System.Collections.Generic
+open System.Linq
+open System.Text
+open Microsoft.SqlServer.Dts.Runtime
+open Microsoft.SqlServer.Dts.Pipeline.Wrapper
+
+let TestPackage = new Package()
+TestPackage.Name   <- "TestPackage"
+
+let app = new Microsoft.SqlServer.Dts.Runtime.Application()
+app.SaveToXml("d:\\test.dtsx",TestPackage,null)
